107974861a4b5692d053cfc48e79828ac73cd76a
Romana-Aijaz <romanaaijaz99@gmail.com>
2023-06-13 22:09:49 +0500

    second commit

---
+++ bank_modern_app/src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar"
+import Clients from "./components/Clients"
+import CTA from "./components/CTA"
+import FeedbackCard from "./components/FeedbackCard"
+import Business from "./components/Business"
+import Billing from "./components/Billing"
+import Stats from "./components/Stats"
+import Testimonials from "./components/Testimonials"
+import Footer from "./components/Footer"
+import Button from "./components/Button"
+import GetStarted from "./components/GetStarted"
+import Hero from "./components/Hero"
+import CardDeal from "./components/CardDeal"
+
+export {
+    Navbar,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Business,
+    Billing,
+    Stats,
+    Testimonials,
+    Footer,
+    Button,
+    GetStarted,
+    Hero,
+    CardDeal
+}
